670abbe7d5729a49e1b5dfc4a74dce1d80034cde
Hélène Le Berre <Hlne Le Berre@DESKTOP-FT60HLT.home>
2018-07-22 16:53:13 +0200

    quimarcheavantdetoutcasseeerrr

--- pyalb/src/Zcasino.py
+++ pyalb/src/Zcasino.py
@@ -1,14 +1,29 @@
 # -*-coding:Latin-1 -*
-
+import math
 from random import randrange
 
-a = int(input("Numero sur lequel parier : "))
-b = int(input("Mise : "))
-c = randrange(50)
-print("le numéro est : ",c)
+b=0
+d=40 #argent
 
-if c == a :
-    b *= 2
-    print("vous avez gagné ",b, "$")
-elif a % 2 == c % 2 :
-    print("todo2")
+while d != 0 :
+    print("vous avez ",d, "$")
+    a = int(input("Numero sur lequel parier : ")) #num    
+    b = int(input("Mise : ")) #Mise
+    if b>d :
+        print("Mise supérieure à votre argent")
+    else :    
+        c = randrange(50) #aléa
+
+        print("le numéro est : ",c)
+
+        if c == a :
+            d += b
+            print("vous avez gagné ",b, "$")
+        elif a % 2 == c % 2 :
+                d += math.ceil(b / 2)
+                print("vous avez gagné", math.ceil(b/2), "$")
+        else :
+            d -= b
+            print("vous avez perdu", b, "$")
+            
+print("Vous n'avez plus d'argent !")
