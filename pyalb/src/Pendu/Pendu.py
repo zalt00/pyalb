93987ba75522dba93b3513a6652b6761d9973f93
Timelam <Timelam@pc-gamer.home>
2018-09-29 11:42:24 +0200

    Pendu 1.0 : fini principe de base, fonctionnel

--- pyalb/src/Pendu/Pendu.py
+++ pyalb/src/Pendu/Pendu.py
@@ -1,17 +1,66 @@
 # -*-coding:Latin-1 -*
 
 from random import randrange
-inlist = list()
+from builtins import enumerate
+import pickle
 
+name = input("Saisir votre prénom : ")
+with open("score", "rb") as fichier :
+    my = pickle.Unpickler(fichier)
+    scoredict = my.load()
+    try :
+        score = scoredict[name]
+    except :
+        scoredict[name] = int(0)
+print("votre score :", scoredict[name])
+     
 with open("donnee.txt", "r") as fichier : ###ligne 5à10 : récupération des données du document txt###
     thgs = fichier.read()
-liste1 = thgs.split("\nEssais = ")
+liste1 = thgs.split("\nEssais =")
 essais = liste1[len(liste1)-1]
 del liste1[len(liste1)-1]
 liste1=liste1[0].split("\n")
+del(thgs) ###variables : essais = nb d'essais ; liste1= liste des mots
 
-a = randrange(len(liste1)) ###coversion du mot choisi en une liste de lettres###
+inlist = list()
+a = randrange(len(liste1)) ###conversion du mot choisi en une liste de lettres###
 mot_choisi = liste1[a]
 for lettre in mot_choisi :
-    inlist += lettre
+    inlist.append(lettre)
+del(lettre)
+del(a) ###inlist = mot converti en lettres ; liste1 = mot aléa ; pré
 
+listaff = list() ###listaff = ******
+for a in inlist :
+    listaff.append("*")
+del(a)
+
+print(*listaff, sep = "")
+essais = int(essais)  ###vif du programme, quand lettre_choisie == une lettre, affichage de la lettre###
+while essais != 0 and "*" in listaff : ###tant que listaff n'est pas pleine et essais != 0
+    lettre_choisie = input() 
+    raté = False 
+    for i,lettre in enumerate(inlist) :
+        if lettre == lettre_choisie :
+            listaff[i] = lettre
+            inlist[i]= "786451"
+            raté = True
+    if raté == False :
+        essais -= 1
+        print("raté ! plus que {} essais !".format(essais))
+    print(*listaff, sep = "")
+    
+score = int()
+if essais == 0 :
+    print("perdu")
+else : 
+    print("vous avez gagné!")
+    scoredict[name] += 1
+    print("votre score :", scoredict[name])
+    with open("score", "wb") as fichier :
+        my = pickle.Pickler(fichier)
+        my.dump(scoredict)
+    
+    
+    
+    
